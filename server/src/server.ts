75504a57f21d640cfb79ef545e00596c0f67d436
Bruno Becoski <brunobecoski@gmail.com>
2022-09-14 17:07:58 -0300

    Server: Criando rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,31 @@
 import express from 'express'
 
-const api = express()
+const app = express()
 
-api.get('/ads', (request, response) => {
-  return response.send(
-    [
-      { id: '1', name: 'Anúncio 1' },
-      { id: '2', name: 'Anúncio 2' },
-      { id: '3', name: 'Anúncio 3' },
-      { id: '4', name: 'Anúncio 4' },
-      { id: '5', name: 'Anúncio 5' },
-    ]
-  );
+app.get('/games', (resquest, response) => {
+  return response.json([])
 })
 
-api.listen(3333)
+app.post('/ads', (resquest, response) => {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id
+
+  return response.json([
+    { id: 1, name: 'Anúncio 1' },
+    { id: 2, name: 'Anúncio 2' },
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' },
+  ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id
+
+  return response.json([])
+})
+
+app.listen(3333)
