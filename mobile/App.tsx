7091ee60db5807353d7ee4091332778617caa9eb
Bruno Becoski <brunobecoski@gmail.com>
2022-09-17 22:23:40 -0300

    Mobile: Configurando notificação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useRef } from 'react';
 import { StatusBar } from 'react-native';
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 import {
   useFonts,
   Inter_400Regular,
@@ -11,6 +14,9 @@ import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 import { Background } from './src/components/Background';
 
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+
 export default function App() {
   const [fontsLoaded ] = useFonts({
     Inter_400Regular,
@@ -18,6 +24,30 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
 
   return (
     <Background>
