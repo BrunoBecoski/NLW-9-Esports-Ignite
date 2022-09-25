e19924fdf4cdbfca5fcad580cf31f0545a101490
Bruno Becoski <brunobecoski@gmail.com>
2022-09-24 23:37:00 -0300

    Web: Carrossel

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import * as Dialog from '@radix-ui/react-dialog';
 
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { CreateAdModal } from './components/CreateAdModal';
@@ -22,6 +25,46 @@ interface Game {
 function App() {
   const [games, setGames] = useState<Game[]>([]);
 
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 6,
+      spacing: 24,
+    },
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
+
+  function Arrow(props: {
+    disabled: boolean
+    left?: boolean
+    onClick: (e: any) => void
+  }) {
+    const disabeld = props.disabled ? " arrow--disabled" : ""
+    return (
+      <svg
+        onClick={props.onClick}
+        className={`arrow ${props.left ? "arrow--left" : "arrow--right"
+          } ${disabeld}`}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        {props.left && (
+          <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+        )}
+        {!props.left && (
+          <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+        )}
+      </svg>
+    )
+  }
+
   useEffect(() => {
     axios('http://localhost:3333/games').then(response => {
       setGames(response.data)
@@ -36,7 +79,8 @@ function App() {
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
       </h1>
 
-      <div className="grid grid-cols-6 gap-6 mt-16">
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
         {games.map(game => {
           return (
             <GameBanner
@@ -47,14 +91,36 @@ function App() {
             />
           )
         })}
-      </div>
+        </div>
+        {loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
 
+            <Arrow
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            />
+          </>
+        )}
+      </div>
+    
       <Dialog.Root>
         <CreateAdBanner />
         
         <CreateAdModal />
       </Dialog.Root>
-    </div>
+    </div >
   )
 }
 
