12d444d2bd57f3a72a8488200b492b81f0b7108b
Bruno Becoski <brunobecoski@gmail.com>
2022-10-02 22:34:04 -0300

    Mobile:  Validando criação de anúncio

--- mobile/src/components/CreateAdModal/indext.tsx
+++ mobile/src/components/CreateAdModal/indext.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Modal, ModalProps, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Picker } from '@react-native-picker/picker';
 import RNDateTimePicker from '@react-native-community/datetimepicker';
 import Checkbox from 'expo-checkbox';
@@ -9,127 +11,102 @@ import { GameCardProps } from '../GameCard';
 
 import { THEME } from '../../theme';
 import { styles } from './styles';
+import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 interface Props extends ModalProps {
   games: GameCardProps[],
 }
 
 export function CreateAdModal({ games, onRequestClose, ...rest }: Props) {
-  const [game, setGame] = useState('');
-  const [weekDays, setWeekDays] = useState([
-    {
-      id: '1',
-      value: 'D',
-      active: false,
-    }, {
-      id: '2',
-      value: 'S',
-      active: false,
-    }, {
-      id: '3',
-      value: 'T',
-      active: false,
-    }, {
-      id: '4',
-      value: 'Q',
-      active: false,
-    }, {
-      id: '5',
-      value: 'Q',
-      active: false,
-    }, {
-      id: '6',
-      value: 'S',
-      active: false,
-    }, {
-      id: '7',
-      value: 'S',
-      active: false,
-    }
-  ]);
-  const [hourStart, setHourStart] = useState({
-    isOpen: false,
-    time: '00:00',
-  });
-  const [hourEnd, setHourEnd] = useState({
-    isOpen: false,
-    time: '00:00',
-  });
-  const [useVoiceChannel, setUseVoiceChannel] = useState(false);
+  const [hourStartPickerIsOpen, setHourStartPickerIsOpen] = useState(false);
+  const [hourEndPickerIsOpen, setHourEndPickerIsOpen] = useState(false);
 
-  function handleRadioButton(id: string) {
-    const weekDaysUpdated = weekDays.map(day => {
-      if (day.id === id) {
-        return {
-          ...day,
-          active: !day.active,
-        }
+  const schema = z.object({
+    game: z.string().uuid('Selecione algum game'),
+    name: z.string().min(1, 'Campo vazio'),
+    yearsPlaying: z.string().min(1, 'Campo vazio'),
+    discord: z.string().min(6, 'Usuário inválido').max(37, 'Usuário inválido').regex(/#[0-9]{4}$/, 'Usuário inválido'),
+    weekDays: z.array(z.string()).nonempty('Selecione algum dia').max(7),
+    hourStart: z.string().regex(/[0-9]{2}:[0-9]{2}/, 'Selecione a hora'),
+    hourEnd: z.string().regex(/[0-9]{2}:[0-9]{2}/, 'Selecione a hora'),
+    useVoiceChannel: z.boolean(),
+  })
+
+  type Schema = z.infer<typeof schema>;
+
+  const { handleSubmit, control, formState: { errors } } = useForm<Schema>({
+    // resolver: zodResolver(schema),
+    defaultValues: {
+      game: '',
+      name: '',
+      yearsPlaying: '',
+      discord: '',
+      weekDays: [],
+      hourStart: '00:00',
+      hourEnd: '00:00',
+      useVoiceChannel: false,
+    }
+  });
+
+  interface RadioButtonProps {
+    onChange: (porps: string[]) => void;
+    index: string;
+    values: string[]; 
+  }
+
+
+  function handleRadioButton({ onChange, index, values }: RadioButtonProps) {
+    if(values.includes(index)) {
+      onChange(values.filter((item) => item !== index));
+    } else {
+      if(values.length) {
+        onChange([...values, index]);
       } else {
-        return day
+        onChange([index]);
       }
-    })
-
-    setWeekDays(weekDaysUpdated);
+    }
   }
 
-  function handleOpenHourStart() {
-    setHourStart({
-      isOpen: true,
-      time: '00:00',
-    });
-  }
-
-  function handleCloseHourStart(data: any) {
-    if (data.type === 'dismissed') {
-      setHourStart({
-        isOpen: false,
-        time: '00:00',
-      });
+  function handleCloseHourStart({ onChange, props }: any) {
+    if (props.type === 'dismissed') {
+      onChange('00:00');
+      setHourStartPickerIsOpen(false);
       return;
     }
 
-    const time = new Date(data.nativeEvent.timestamp);
+    const time = new Date(props.nativeEvent.timestamp);
 
     const hours = String(time.getHours());
     const minutes = String(time.getMinutes());
 
     const timeFormatted = `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}`;
 
-    setHourStart({
-      isOpen: false,
-      time: timeFormatted,
-    });
+    onChange(timeFormatted)
+    setHourStartPickerIsOpen(false);
   }
 
-  function handleOpenHourEnd() {
-    setHourEnd({
-      isOpen: true,
-      time: '00:00',
-    });
-  }
-
-  function handleCloseHourEnd(data: any) {
-    if (data.type === 'dismissed') {
-      setHourEnd({
-        isOpen: false,
-        time: '00:00',
-      });
+  function handleCloseHourEnd({ onChange, props }: any) {
+    if (props.type === 'dismissed') {
+      onChange('00:00');
+      setHourEndPickerIsOpen(false);
       return;
     }
 
-    const time = new Date(data.nativeEvent.timestamp);
+    const time = new Date(props.nativeEvent.timestamp);
 
     const hours = String(time.getHours());
     const minutes = String(time.getMinutes());
 
     const timeFormatted = `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}`;
 
-    setHourEnd({
-      isOpen: false,
-      time: timeFormatted,
-    });
+    onChange(timeFormatted);
+    setHourEndPickerIsOpen(false);
   }
 
+  function onSubmit(data: Schema) {
+    console.log(data);
+  }
 
   return (
     <Modal
@@ -144,66 +121,147 @@ export function CreateAdModal({ games, onRequestClose, ...rest }: Props) {
 
         <View style={styles.wrapper}>
           <Text style={styles.label}>Qual o game?</Text>
-          <Picker
-            style={styles.picker}
-            selectedValue={game}
-            onValueChange={(itemValue) => setGame(itemValue)}
-          >
-            {games.map(game => {
-              return (
+          <Controller
+            control={control}
+            name="game"
+            render={({ field: { onChange, value} }) => (
+              <Picker
+                style={styles.picker}
+                selectedValue={value}
+                onValueChange={onChange}
+                dropdownIconColor={THEME.COLORS.TEXT}
+              >
                 <Picker.Item
-                  key={game.id}
+                  label="Selecione o game que deseja jogar"
                   style={styles.pickerItem}
-                  label={game.title}
-                  value={game.id}
+                  enabled={false}
                 />
-              )
-            })}
-          </Picker>
+                {games.map(game => {
+                  return (
+                    <Picker.Item
+                      key={game.id}
+                      style={styles.pickerItem}
+                      label={game.title}
+                      value={game.id}
+                    />
+                  )
+                })}
+              </Picker>
+            )}
+          />
         </View>
 
         <View style={styles.wrapper}>
           <Text style={styles.label}>Seu nome (ou nickname)</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Como te chamam dentro do game?"
-            placeholderTextColor={THEME.COLORS.CAPTION_500}
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value} }) => (
+              <TextInput
+                style={styles.input}
+                placeholder="Como te chamam dentro do game?"
+                placeholderTextColor={THEME.COLORS.CAPTION_500}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
         </View>
 
         <View style={styles.wrapper}>
           <Text style={styles.label}>Joga há quantos anos?</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Tudo bem ser ZERO"
-            placeholderTextColor={THEME.COLORS.CAPTION_500}
+          <Controller
+            control={control}
+            name="yearsPlaying"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                keyboardType='numeric'
+                style={styles.input}
+                placeholder="Tudo bem ser ZERO"
+                placeholderTextColor={THEME.COLORS.CAPTION_500}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
         </View>
 
         <View style={styles.wrapper}>
           <Text style={styles.label}>Qual seu Discord?</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Usuario#0000"
-            placeholderTextColor={THEME.COLORS.CAPTION_500}
+          <Controller
+            control={control}
+            name="discord"
+            render={({ field: { onChange, value} }) => (
+              <TextInput
+                style={styles.input}
+                placeholder="Usuario#0000"
+                placeholderTextColor={THEME.COLORS.CAPTION_500}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
         </View>
 
         <View style={styles.wrapper}>
           <Text style={styles.label}>Quando costuma jogar?</Text>
-          <View style={styles.radioWrapper}>
-            {weekDays.map(day => {
-              return (
-                <TouchableOpacity
-                  key={day.id}
-                  onPress={() => handleRadioButton(day.id)}
-                  style={day.active ? styles.radioButtonActive : styles.radioButtonInactive}
-                >
-                  <Text style={styles.radioButtonTitle}>{day.value}</Text>
-                </TouchableOpacity>
-              )
-            })}
-          </View>
+            <Controller
+              name="weekDays"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <View style={styles.radioWrapper}>
+                  <TouchableOpacity
+                    onPress={() => handleRadioButton({onChange, index: '0', values: value})}
+                    style={value.includes('0') ? styles.radioButtonActive : styles.radioButtonInactive}
+                  >
+                    <Text style={styles.radioButtonTitle}>D</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => handleRadioButton({onChange, index: '1', values: value}) }
+                    style={value.includes('1') ? styles.radioButtonActive : styles.radioButtonInactive}
+                  >
+                    <Text style={styles.radioButtonTitle}>S</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => handleRadioButton({onChange, index: '2', values: value})}
+                    style={value.includes('2') ? styles.radioButtonActive : styles.radioButtonInactive}
+                  >
+                    <Text style={styles.radioButtonTitle}>T</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => handleRadioButton({onChange, index: '3', values: value})}
+                    style={value.includes('3') ? styles.radioButtonActive : styles.radioButtonInactive}
+                  >
+                    <Text style={styles.radioButtonTitle}>Q</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => handleRadioButton({onChange, index: '4', values: value})}
+                    style={value.includes('4') ? styles.radioButtonActive : styles.radioButtonInactive}
+                  >
+                    <Text style={styles.radioButtonTitle}>Q</Text>
+                  </TouchableOpacity>
+
+
+                  <TouchableOpacity
+                    onPress={() => handleRadioButton({onChange, index: '5', values: value})}
+                    style={value.includes('5') ? styles.radioButtonActive : styles.radioButtonInactive}
+                  >
+                    <Text style={styles.radioButtonTitle}>S</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    onPress={() => handleRadioButton({onChange, index: '6', values: value})}
+                    style={value.includes('6') ? styles.radioButtonActive : styles.radioButtonInactive}
+                  >
+                    <Text style={styles.radioButtonTitle}>S</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            />
         </View>
 
         <View style={styles.wrapper}>
@@ -211,49 +269,67 @@ export function CreateAdModal({ games, onRequestClose, ...rest }: Props) {
           <View style={styles.timeWrapper}>
             <View style={styles.time}>
               <Text style={styles.label}>De</Text>
-              <TouchableOpacity
-                onPress={handleOpenHourStart}
-                style={styles.timeButton}
-              >
-                <Text style={styles.timeButtonTitle}>{hourStart.time}</Text>
-                <Clock size={16} color={THEME.COLORS.TEXT} />
-              </TouchableOpacity>
-              {
-                hourStart.isOpen &&
-                <RNDateTimePicker
-                  value={new Date()}
-                  mode="time"
-                  onChange={handleCloseHourStart}
-                />
-              }
+              <Controller
+                name="hourStart"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TouchableOpacity
+                    onPress={() => setHourStartPickerIsOpen(true)}
+                    style={styles.timeButton}
+                  >
+                    <Text style={styles.timeButtonTitle}>{value}</Text>
+                    <Clock size={16} color={THEME.COLORS.TEXT} />
+                    {
+                      hourStartPickerIsOpen &&
+                      <RNDateTimePicker
+                        value={new Date()}
+                        mode="time"
+                        onChange={(props) => handleCloseHourStart({onChange, props})}
+                      />
+                    }
+                  </TouchableOpacity>
+                )}
+              />
             </View>
 
             <View style={styles.time}>
               <Text style={styles.label}>Até</Text>
-              <TouchableOpacity
-                onPress={handleOpenHourEnd}
-                style={styles.timeButton}
-              >
-                <Text style={styles.timeButtonTitle}>{hourEnd.time}</Text>
-                <Clock size={16} color={THEME.COLORS.TEXT} />
-              </TouchableOpacity>
-              {
-                hourEnd.isOpen &&
-                <RNDateTimePicker
-                  value={new Date()}
-                  mode="time"
-                  onChange={handleCloseHourEnd}
-                />
-              }
+              <Controller
+                name="hourEnd"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TouchableOpacity
+                    onPress={() => setHourEndPickerIsOpen(true)}
+                    style={styles.timeButton}
+                    >
+                    <Text style={styles.timeButtonTitle}>{value}</Text>
+                    <Clock size={16} color={THEME.COLORS.TEXT} />
+                    {
+                      hourEndPickerIsOpen &&
+                      <RNDateTimePicker
+                        value={new Date()}
+                        mode="time"
+                        onChange={(props) => handleCloseHourEnd({onChange, props})}
+                      />
+                    }
+                  </TouchableOpacity>
+                )}
+              />
             </View>
           </View>
         </View>
 
         <View style={styles.checkboxWrapper} >
-          <Checkbox
-            style={styles.checkbox}
-            value={useVoiceChannel}
-            onValueChange={setUseVoiceChannel}
+          <Controller
+            name="useVoiceChannel"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Checkbox
+                style={styles.checkbox}
+                onValueChange={onChange}
+                value={value}
+              />
+            )}
           />
           <Text style={styles.checkboxTitle}>Costumo me conectar ao chat de voz</Text>
         </View>
@@ -265,7 +341,10 @@ export function CreateAdModal({ games, onRequestClose, ...rest }: Props) {
             <Text style={styles.cancelButtonTitle}>Cancelar</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.submitButton}>
+          <TouchableOpacity 
+            onPress={handleSubmit(onSubmit)}
+            style={styles.submitButton}
+          >
             <GameController size={24} color={THEME.COLORS.TEXT} />
             <Text style={styles.submitButtonTitle}>Encontrar duo</Text>
           </TouchableOpacity>
